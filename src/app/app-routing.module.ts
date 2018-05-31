e7ca586cedc4e264053d5cecbdf7ec985e59b044
Csaba Todor <csaba.todor@outlook.com>
2018-05-31 23:54:11 +0300

    Added Routing for FruitsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './ui/home/home.component';
 import { AuthComponent } from './ui/auth/auth.component';
 import { ContactComponent } from './ui/contact/contact.component';
+import { FruitsComponent } from './ui/fruits/fruits.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'authentication', component: AuthComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'fruits', component: FruitsComponent },
 ];
 
 @NgModule({
