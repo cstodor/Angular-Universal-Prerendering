730cd36a1d19bef851f8a717a7d1794b340ef6b7
Csaba Todor <csaba.todor@outlook.com>
2018-05-31 23:52:26 +0300

    Imported/Declared: Header, Footer & PrimaryMenu Components and Exported: Header & Footer Components

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AuthComponent } from './auth/auth.component';
 import { FruitsComponent } from './fruits/fruits.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PrimaryMenuComponent } from './shared/primary-menu/primary-menu.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  declarations: [HomeComponent, ContactComponent, AuthComponent, FruitsComponent]
+  declarations: [
+    HomeComponent,
+    ContactComponent,
+    AuthComponent,
+    FruitsComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrimaryMenuComponent
+  ],
+  exports: [HeaderComponent, FooterComponent]
 })
 export class UiModule { }
