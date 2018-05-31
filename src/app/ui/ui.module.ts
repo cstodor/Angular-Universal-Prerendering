23fbb276843d1ade4f22e4013d9260f26ab846a0
Csaba Todor <csaba.todor@outlook.com>
2018-05-31 23:04:34 +0300

    Generated and Declared FruitsComponent

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AuthComponent } from './auth/auth.component';
+import { FruitsComponent } from './fruits/fruits.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HomeComponent, ContactComponent, AuthComponent]
+  declarations: [HomeComponent, ContactComponent, AuthComponent, FruitsComponent]
 })
 export class UiModule { }
