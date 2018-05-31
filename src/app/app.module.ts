1dfe8d44386acbe7b06322c080aa436779edd2bb
Csaba Todor <csaba.todor@outlook.com>
2018-05-31 23:53:28 +0300

    Imported AngularFireStore Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// AngularFire2 Modules
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireFunctionsModule } from 'angularfire2/functions';
+
+import { environment } from '../environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { UiModule } from './ui/ui.module';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -12,6 +23,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserTransferStateModule,
+    AngularFireModule.initializeApp(environment.firebase, 'angular-core-application'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireFunctionsModule,
     AppRoutingModule,
     UiModule
   ],
